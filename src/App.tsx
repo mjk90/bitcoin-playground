69f79e78a38ddad112bb868433c7e47e69c1b03d
Matthew Kelly <mkelly2@live.co.uk>
2021-11-16 17:31:13 +0800

    Added footer

--- src/App.tsx
+++ src/App.tsx
@@ -19,12 +19,12 @@ function App() {
   const { data: { tab }, error, loading }: NavState = useSelector((state: RootState) => state.nav);
 
   return (
-    <div className="App px-4">
+    <div className="App px-4 bg-gray-100 h-screen pb-4">
       <header className="App__Header">
-        <h1 className="text-6xl font-normal leading-normal mt-0 mb-2 text-purple-800">Bitcoin Playground</h1>
+        <h1 className="text-green-500">Bitcoin Playground</h1>
       </header>
       <div className="App__Body max-w-screen-lg block mx-auto">
-        <div className="flex justify-center h-screen">
+        <div className="flex justify-center">
           <div className="w-full">
             <ul className="flex justify-center items-center my-4">
               {tabs.map((t: any, i: number) => 
@@ -33,7 +33,7 @@ function App() {
                   onClick={() => dispatch(setTab(t.index))}>{t.title}</li>
               )}
             </ul>
-            <div className="bg-white p-4 sm:p-8 text-left mx-auto border">
+            <div className="p-4 sm:p-8 text-left mx-auto border bg-white shadow-sm">
               {
                 tab === 0 ? <div><MnemonicWords /></div> :
                 tab === 1 ? <div><SegWit /></div> :
@@ -44,6 +44,7 @@ function App() {
           </div>
         </div>
       </div>
+      <footer className="App__Footer w-full text-center inset-x-0 bottom-0 fixed p-4 hidden sm:block"><a href="https://mjk90.github.io/" target="_blank">Matt Kelly</a> 2021</footer>
       <Popup />
     </div>
   );
